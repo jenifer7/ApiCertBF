8ab788460ad93d880de85fc2c246185edceb9c19
jenifer7 <jeni9jacinto7p@gmail.com>
2020-10-07 04:15:13 -0600

    product type controller added

---
+++ app/controllers/product_type_controller.js
@@ -0,0 +1,99 @@
+const ProductType = require("../models/ProductType.js");
+
+//Crear un nuevo usuario y guardarlo
+exports.create = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: "No se puede enviar contenido vacio"
+        });
+    }
+    const productType = new ProductType({
+        type: req.body.type,
+        description: req.body.description,
+        is_active: req.body.is_active
+    });
+
+    ProductType.create(productType, (err, data) => {
+        if(err)
+        res.status(500).send({
+            message: err.message || "Error al crear registro en tipos de producto"
+        });
+        else res.send(data);
+    });
+};
+
+
+//Mostrar todos los registros
+exports.findAll = (req, res) => {
+    ProductType.getAll((err, data) => {
+        if(err)
+            res.status(500).send({
+                message:
+                err.message || "Error al intentar mostrar los registros de Tipos de producto"
+            });
+            else res.send(data);
+    });
+};
+
+
+//Mostrar un usuario segun id
+exports.findById = (req, res) => {
+    ProductType.findById(req.params.productTypeId, (err, data) => {
+        if(err) {
+            if(err.kind === "No encontrado") {
+                res.status(404).send({
+                    message: 'No se encontro el tipo de producto ${req.params.productTypeId}'
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error al mostrar el registro " + req.params.productTypeId
+                });
+            }
+        }else res.send(data);
+    });
+};
+
+
+
+//Editar registros y guardar los cambios
+exports.update = (req, res) => {
+    if(!req.body) {
+        res.status(400).send({
+            message: "No se puede ingresar campos vacios"
+        });
+    }
+
+    ProductType.update(req.params.productTypeId, new ProductType(req.body),
+        (err, data) => {
+            if (err) {
+                if (err.kind === "No encontrado") {
+                    res.status(404).send({
+                        message: 'Tipo de producto no encontrado ${req.params.productTypeId}'
+                    });
+                }else {
+                    res.status(500).send({
+                        message: "Error al actualizar registro " + req.params.productTypeId
+                    });
+                }
+            }else res.send(data);
+        });
+};
+
+
+
+//Eliminar un registro
+exports.delete = (req, res) => {
+    ProductType.remove(req.params.productTypeId, (err, data) => {
+        if (err) {
+            if (err.kind === "No disponible") {
+                res.status(404).send({
+                    message: 'Tipo de producto no encontrado ${req.params.productTypeId}'
+                });
+            } else {
+                res.status(500).send({
+                    message: "No se pudo eliminar el registro " + req.params.productTypeId
+                });
+            }
+        }else res.send({ message: "Registro eliminado correctamente" });
+    });
+};
