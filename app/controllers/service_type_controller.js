8acb75e77d3c99fd0926a3f2c5523d0bb4b1f452
jenifer7 <jeni9jacinto7p@gmail.com>
2020-10-07 04:16:27 -0600

    service type controller added

---
+++ app/controllers/service_type_controller.js
@@ -0,0 +1,99 @@
+const ServiceType = require("../models/ServiceType.js");
+
+//Crear un nuevo tipos de servicio y guardarlo
+exports.create = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: "No se puede enviar contenido vacio"
+        });
+    }
+    const serviceType = new ServiceType({
+        name: req.body.name,
+        description: req.body.description,
+        is_active: req.body.is_active
+    });
+
+    ServiceType.create(serviceType, (err, data) => {
+        if(err)
+        res.status(500).send({
+            message: err.message || "Error al crear registro de tipos de servicio"
+        });
+        else res.send(data);
+    });
+};
+
+
+//Mostrar todos los registros
+exports.findAll = (req, res) => {
+    ServiceType.getAll((err, data) => {
+        if(err)
+            res.status(500).send({
+                message:
+                err.message || "Error al intentar mostrar los registros de tipos de servicios"
+            });
+            else res.send(data);
+    });
+};
+
+
+//Mostrar un tipo de servicio segun id
+exports.findById = (req, res) => {
+    ServiceType.findById(req.params.serviceTypeId, (err, data) => {
+        if(err) {
+            if(err.kind === "No encontrado") {
+                res.status(404).send({
+                    message: 'No se encontro el tipos de servicio ${req.params.serviceTypeId}'
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error al mostrar el registro " + req.params.serviceTypeId
+                });
+            }
+        }else res.send(data);
+    });
+};
+
+
+
+//Editar registros y guardar los cambios
+exports.update = (req, res) => {
+    if(!req.body) {
+        res.status(400).send({
+            message: "No se puede ingresar campos vacios"
+        });
+    }
+
+    ServiceType.update(req.params.serviceTypeId, new ServiceType(req.body),
+        (err, data) => {
+            if (err) {
+                if (err.kind === "No encontrado") {
+                    res.status(404).send({
+                        message: 'tipos de servicio no encontrado ${req.params.serviceTypeId}'
+                    });
+                }else {
+                    res.status(500).send({
+                        message: "Error al actualizar registro " + req.params.serviceTypeId
+                    });
+                }
+            }else res.send(data);
+        });
+};
+
+
+
+//Eliminar un registro
+exports.delete = (req, res) => {
+    ServiceType.remove(req.params.serviceTypeId, (err, data) => {
+        if (err) {
+            if (err.kind === "No disponible") {
+                res.status(404).send({
+                    message: 'tipos de servicio no encontrado ${req.params.serviceTypeId}'
+                });
+            } else {
+                res.status(500).send({
+                    message: "No se pudo eliminar el usuario " + req.params.serviceTypeId
+                });
+            }
+        }else res.send({ message: "tipos de servicio eliminado correctamente" });
+    });
+};
