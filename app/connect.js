879266ae96c1dab31f3055594fb7cbe07d3e48fb
jenifer7 <jeni9jacinto7p@gmail.com>
2020-10-07 04:08:35 -0600

    connection file

---
+++ app/connect.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql');
+const dbConfig = require('./config/database');
+
+// const { promisify } = require('util');
+// const pool = mysql.createPool(dbConfig);
+
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//             console.error('Database connection was closed');
+//         }
+//         if (err.code === 'ER_CON_COUNT_ERROR') {
+//             console.error('Database has to many connections');
+//         }
+//         if (err.code === 'ECONNREFUSED') {
+//             console.error('Database connection was refused');
+//         }
+//     }
+//     if (connection) connection.release();
+//     console.log('Database is connected');
+//     return;
+// });
+
+const connection = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+});
+
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Base de datos conectada');
+});
+
+// pool.query = promisify(pool.query);
+
+module.exports = connection;
+// module.exports = pool;
